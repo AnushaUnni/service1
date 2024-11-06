365090ece898bb963314e08154c68567fb55f754
AnushaUnni <anushaunni.auau@gmail.com>
2024-11-06 17:15:29 +0800

    Update redis config to only have address

--- lib/calculate/configs/redis_config.go
+++ lib/calculate/configs/redis_config.go
@@ -1,17 +1,11 @@
 package configs
 
 type RedisConfig struct {
-	RedisAddr     string // Redis server address
-	RedisPassword string // Redis password (if needed)
-	RedisDB       int    // Redis database index (default is 0)
-	PoolSize      int    // Number of connections in the Redis connection pool
+	RedisAddr string // Redis server address
 }
 
-func NewRedisConfig(redisAddr string, redisPassword string, redisDB int, poolSize int) *RedisConfig {
+func NewRedisConfig(redisAddr string) *RedisConfig {
 	return &RedisConfig{
-		RedisAddr:     redisAddr,
-		RedisPassword: redisPassword,
-		RedisDB:       redisDB,
-		PoolSize:      poolSize,
+		RedisAddr: redisAddr,
 	}
 }
